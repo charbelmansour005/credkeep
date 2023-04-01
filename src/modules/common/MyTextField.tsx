d27fa2bd07ebba5a0c90d41b93ab288ae07b2019
charbelmansour005 <charbelmansour005@gmail.com>
2023-04-02 01:16:20 +0300

    added <> reusable UI textfield

---
+++ src/modules/common/MyTextField.tsx
@@ -0,0 +1,34 @@
+import React, { ChangeEvent } from "react"
+import { TextField } from "@mui/material"
+
+interface MyTextFieldProps {
+  id: string
+  label: string
+  value: any
+  onChange: (value: string) => void
+}
+
+export const MyTextField: React.FC<MyTextFieldProps> = ({
+  id,
+  label,
+  value,
+  onChange,
+}) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value
+    onChange(value)
+  }
+
+  return (
+    <TextField
+      sx={{ m: 1.5 }}
+      id={id}
+      label={label}
+      variant="outlined"
+      size="small"
+      fullWidth
+      onChange={handleChange}
+      value={value}
+    />
+  )
+}

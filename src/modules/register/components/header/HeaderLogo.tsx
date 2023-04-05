3252eec8267cf0420c17b640cfe7031f2660f8bd
charbelmansour005 <charbelmansour005@gmail.com>
2023-04-06 00:31:10 +0300

    fixed <> logo styling

--- src/modules/register/components/header/HeaderLogo.tsx
+++ src/modules/register/components/header/HeaderLogo.tsx
@@ -2,6 +2,16 @@ import Logo from "assets/logo0.png"
 
 export default function HeaderLogo() {
   return (
-    <img alt="logo" src={Logo} style={{ height: "220px", width: "220px" }} />
+    <img
+      alt="logo"
+      src={Logo}
+      style={{
+        height: "285px",
+        width: "285px",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: "100%",
+      }}
+    />
   )
 }
